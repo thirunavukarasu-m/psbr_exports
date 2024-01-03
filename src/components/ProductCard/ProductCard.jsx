664a8b37014aeb6c21757e3d97a2843839fd5391
Thirunavukarasu M <68892257+thirunavukarasu-m@users.noreply.github.com>
2024-01-03 19:01:54 +0530

    Added contents in home page.

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./ProductCard.css"
+import { Link } from 'react-router-dom'
+
+const ProductCard = ({ product }) => {
+    return (
+        <>
+            <Link to="/products">
+                <div className="product-card">
+                    <h3 className='text-center fs-3 pt-2'>{product.heading}</h3>
+                    <img src={product.img} alt="" />
+                </div>
+
+            </Link>
+        </>
+    )
+}
+
+export default ProductCard
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
 import './HomePage.css'
 import Banner from '../../components/Banner/Banner'
+import SingleProductInfo from '../../components/SingleProductInfo/SingleProductInfo'
+import ship from '../../assets/ship_portrait.jpg'
+import black_pepper from '../../assets/products/black-pepper.jpg'
+import cardamom from '../../assets/products/cardamom.jpg'
+import cinnamon from '../../assets/products/cinnamon.jpg'
+import cloves from '../../assets/products/cloves.jpg'
+import cumin from '../../assets/products/cumin.jpg'
+import red_chilli from '../../assets/products/red-chilli.jpg'
+import coriander from '../../assets/products/coriander.jpg'
+import tamarind from '../../assets/products/tamarind.jpg'
+import turmaric from '../../assets/products/turmaric.jpg'
+import ProductCard from '../../components/ProductCard/ProductCard'
 const HomePage = () => {
+  const products = [
+    {
+      heading: "Black Pepper",
+      img: black_pepper
+    },
+    {
+      heading: "Cardamom",
+      img: cardamom
+    },
+    {
+      heading: "Cinnamon",
+      img: cinnamon
+    },
+    {
+      heading: "Cloves",
+      img: cloves
+    },
+    {
+      heading: "Cumin",
+      img: cumin
+    },
+    {
+      heading: "Red Chilli",
+      img: red_chilli
+    },
+    {
+      heading: "Coriander",
+      img: coriander
+    },
+    {
+      heading: "Tamarind",
+      img: tamarind
+    },
+    {
+      heading: "Turmaric",
+      img: turmaric
+    },
+  ]
   return (
     <>
-      <Banner/>
+      <Banner />
+      <h1 className='text-center pt-5'> Who we are</h1>
+      <SingleProductInfo product={ship} />
+      <h2 className='text-center pt-5'> Explore </h2>
+      <h1 className='text-center pt-4'> Our Products </h1>
+      <div className="products-card-container d-flex pt-4 mb-5">
+        {
+          products.map((product)=>{
+            return(
+              <>
+                <ProductCard product={product}/>
+              </>
+            )
+          })
+        }
+      </div>
     </>
   )
 }
