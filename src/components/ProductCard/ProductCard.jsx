e79f3ec793d5bf1bbe7766f4ef16615903554da9
Thirunavukarasu M <mailme.thirunavukarasu@gmail.com>
2024-03-10 16:59:36 +0530

    Modified the product card.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -35,20 +35,23 @@ const ProductCard = ({ product, index }) => {
 
     return (
         <>
-            <Link to={product.heading.split(" ")[1] ? "/products#" + product.heading.split(" ")[1] : "/products#" + product.heading} onClick={scrollToTop}
+            <Link className='product-card-text' to={product.heading.split(" ")[1] ? "/products#" + product.heading.split(" ")[1] : "/products#" + product.heading} onClick={scrollToTop}
 
             >
                 {showSkeleton && <SkeletonLoader height={300} width={350} />}
 
                 {showImage && (
-                    <m.div className="product-card"
-                        initial={{ opacity: 0,x:"100%" }}
-                        whileInView={{ opacity: 1,x:"0%" }}
-                        transition={{ duration: 0.75, ease: "easeOut",delay:index * 0.2 }}
+                    <m.div className="product-card d-flex flex-column align-items-center"
+                        initial={{ opacity: 0, x: "100%" }}
+                        whileInView={{ opacity: 1, x: "0%" }}
+                        transition={{ duration: 0.5, ease: "easeOut" }}
                         viewport={{ once: true }}
                     >
-                        <h3 className='text-center fs-3 pt-2 blue-text'>{product.heading}</h3>
                         <img src={product.img} alt="" />
+                        <div className="mt-4" >
+                            <h3 className='text-center'>{product.heading}</h3>
+                            <h6 className='text-center'>Read more {'>>'}</h6>
+                        </div>
                     </m.div>
                 )}
             </Link>
